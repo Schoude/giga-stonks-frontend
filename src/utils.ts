4eb5ec70575c895b5802680a2d4306a384231c92
Marc <marcbaque1311@gmail.com>
2023-02-26 12:39:39 +0100

    [ ADD ] date transformer fn

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,16 @@ export function getISODate() {
   date.setDate(date.getDate() - 1);
   return date.toISOString().slice(0, 10).replace(/-/g, "");
 }
+
+/**
+ *
+ * @param isoString '20230215'
+ * @returns '2023-02-15'
+ */
+export function ISODateToJSDate(isoString: string) {
+  const year = isoString.slice(0, 4);
+  const month = isoString.slice(4, 6);
+  const day = isoString.slice(6, 8);
+
+  return `${year}-${month}-${day}`;
+}
