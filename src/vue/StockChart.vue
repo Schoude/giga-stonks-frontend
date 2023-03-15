6f59d811080a959ecbe8d79fb2123e6aaa510dc7
Marc <marcbaque1311@gmail.com>
2023-03-15 22:44:22 +0100

    [ ADD ] initial interval changer

--- src/vue/StockChart.vue
+++ src/vue/StockChart.vue
@@ -1,40 +1,106 @@
 <script setup lang='ts'>
-import { reactive, ref } from 'vue';
+import { computed, reactive, ref, watchEffect } from 'vue';
 import type { Candle, FHCandles, IntervalValues} from '../types/stock-chart';
 import { getInterval, collectCandleData } from '../utils';
 import StockChartRenderer from './StockChartRenderer.vue';
 
 const props = defineProps<{
   symbol: string;
-  interval: IntervalValues;
 }>();
 
+const interval = ref<IntervalValues>('days-1')
+const isIntervalActive = computed(() => {
+  return (givenInterval: IntervalValues) => {
+    return interval.value === givenInterval;
+  }
+});
 const chartSetup = reactive<{
   data: Candle[];
 }>({
-  data: [],
+  data: [{
+    c: 0,
+    o: 0,
+    h: 0,
+    l: 0,
+    v: 0,
+    time: new Date(),
+  }],
 });
 
 const factor = 1;
 const width = 1270 * factor;
-const height = 450 * factor;
+const height = 425 * factor;
 const margin = {
   top: 0,
   right: 65,
   bottom: 25,
-  left: 50,
+  left: 60,
 };
 
-const {from, to} = getInterval(props.interval);
-
 const CANDLES = ref<FHCandles | null>(null);
-CANDLES.value = await (await fetch(`https://finnhub.io/api/v1/stock/candle?symbol=${props.symbol}&resolution=1&from=${from}&to=${to}&token=cfje729r01que34nv410cfje729r01que34nv41g`)).json() as FHCandles;
 
-chartSetup.data = collectCandleData(CANDLES.value);
+watchEffect(async () => {
+  const {from, to} = getInterval(interval.value);
+  CANDLES.value = await (await fetch(`https://finnhub.io/api/v1/stock/candle?symbol=${props.symbol}&resolution=1&from=${from}&to=${to}&token=cfje729r01que34nv410cfje729r01que34nv41g`)).json() as FHCandles;
+  chartSetup.data = collectCandleData(CANDLES.value);
+});
 </script>
 
 <template>
+  <div class="interactions">
+    <div class="intervals">
+      <button
+        class="btn-interval"
+        :disabled="isIntervalActive('week')"
+        title="Week"
+        @click="interval = 'week'"
+      >
+        W
+      </button>
+      <button
+        class="btn-interval"
+        :disabled="isIntervalActive('days-2')"
+        title="Two days"
+        @click="interval = 'days-2'"
+      >
+        D-2
+      </button>
+      <button
+        class="btn-interval"
+        :disabled="isIntervalActive('days-1')"
+        title="One day"
+        @click="interval = 'days-1'"
+      >
+        D-1
+      </button>
+      <button
+        class="btn-interval"
+        :disabled="isIntervalActive('hours-3')"
+        title="Three hours"
+        @click="interval = 'hours-3'"
+      >
+        H-3
+      </button>
+      <button
+        class="btn-interval"
+        :disabled="isIntervalActive('hours-1')"
+        title="One hour"
+        @click="interval = 'hours-1'"
+      >
+        H-1
+      </button>
+      <button
+        class="btn-interval"
+        :disabled="isIntervalActive('minutes-30')"
+        title="Thirty minutes"
+        @click="interval = 'minutes-30'"
+      >
+        M-30
+      </button>
+    </div>
+  </div>
   <StockChartRenderer
+    v-if="chartSetup.data[0].c > 0"
     :data="chartSetup.data"
     :height="height"
     :width="width"
@@ -42,4 +108,17 @@ chartSetup.data = collectCandleData(CANDLES.value);
   />
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.intervals {
+  display: flex;
+  gap: .5rem;
+}
+
+.btn-interval {
+  border: none;
+  padding: .25rem .5rem;
+  line-height: 1;
+  border-radius: var(--border-radius);
+  background-color: #0b121d;
+}
+</style>
--- src/vue/StockChartLoader.vue
+++ src/vue/StockChartLoader.vue
@@ -1,20 +1,16 @@
 <script setup lang='ts'>
 import { ref } from 'vue';
-import type { IntervalValues} from '../types/stock-chart';
 import StockChart from './StockChart.vue';
 
 defineProps<{
   symbol: string;
 }>();
-
-const interval = ref<IntervalValues>('days-1')
 </script>
 
 <template>
   <div class="stock-chart-loader container outlined">
     <Suspense>
-      <StockChart :symbol="symbol" :interval="interval" />
-
+      <StockChart :symbol="symbol" />
       <template #fallback>
         <p>Loading Chart Data...</p>
       </template>
