0fcfa269be93be4d992eaeb8fca5ca9dd47d129f
Marc <marcbaque1311@gmail.com>
2023-03-15 20:56:14 +0100

    [ ADD ] vue integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import deno from '@astrojs/deno';
+import vue from "@astrojs/vue";
 
 export default defineConfig({
   output: 'server',
-  adapter: deno()
+  adapter: deno(),
+  integrations: [
+    vue(),
+  ]
 });
