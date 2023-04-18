dc2d54681c43310642ffa9f8b4570812daa0f493
Marc <marcbaque1311@gmail.com>
2023-04-18 23:17:16 +0200

    [ ADD ] wrapper component for selecting a symbol for detailed earnings data

---
+++ src/vue/EPSDataContainer.vue
@@ -0,0 +1,28 @@
+<script setup lang='ts'>
+import { ref } from 'vue';
+import SymbolSearch from './SymbolSearch.vue';
+
+const selectedSymbol = ref<null | {name: string; symbol: string;}>(null);
+</script>
+
+<template>
+<section class="eps-data-container container outlined">
+  <h1>Earnings Data</h1>
+  <div class="search-container">
+    <SymbolSearch  @symbol-selected="payload => selectedSymbol = payload"/>
+      <h2 class="symbol-name">{{ selectedSymbol?.name}}</h2>
+  </div>
+</section>
+</template>
+
+<style lang='scss' scoped>
+.search-container {
+  display: flex;
+  align-items: flex-end;
+  gap: 2rem;
+}
+
+.symbol-name {
+  margin: 0;
+}
+</style>
