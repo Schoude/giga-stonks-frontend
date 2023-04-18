a7b78f6a7154bd3f0959756a9a33d4669374b2fb
Marc <marcbaque1311@gmail.com>
2023-04-19 00:10:56 +0200

    [ ADD ] basic EPS surprises data display

--- src/vue/EPSSurprises.vue
+++ src/vue/EPSSurprises.vue
@@ -26,10 +26,44 @@ watch(() => props.symbol, async (newValue) => {
     <p>Lade Daten...</p>
   </template>
   <template v-else-if="data">
-    <p>{{ data[0] }}</p>
+    <div class="data-display">
+      <dl class="entry" v-for="entry in data" :key="`${entry.year}-${entry.quarter}`">
+        <dt>Peroid</dt>
+        <dd>{{ entry.period }}</dd>
+        <dt>Quarter</dt>
+        <dd>{{ entry.quarter }}</dd>
+        <dt>Estimate</dt>
+        <dd>{{ entry.estimate }}</dd>
+        <dt>Actual</dt>
+        <dd>{{ entry.actual }}</dd>
+        <dt>Surprise absolute</dt>
+        <dd>{{ entry.surprise }}</dd>
+        <dt>Surprise percent</dt>
+        <dd>{{ entry.surprisePercent }}</dd>
+      </dl>
+    </div>
   </template>
 </div>
 </template>
 
 <style lang='scss' scoped>
+.title {
+  margin-block-end: 1rem;
+  font-size: 1.35rem;
+}
+
+.data-display {
+  display: flex;
+  gap: 2rem;
+}
+
+.entry {
+  dt {
+    font-weight: 700;
+  }
+
+  dd {
+    margin-inline-start: 40px;
+  }
+}
 </style>
